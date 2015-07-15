762fff22883cb7ef85365d2ef4800c185164e91a
nystromb <bnystrom@siu.edu>
2015-07-15 12:46:39 -0400

    start function to run game loop

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,26 +3,25 @@ require "./lib/GameBoard.rb"
 require "./lib/GameModel.rb"
 require "./lib/GamePrompt.rb"
 
+def start(game)
+  until game.is_over
+    game.make_move
+  end
+end
+
 if __FILE__ == $0
   gp = Prompt.new
-  #check for game settings
+  
+  #check for command line args
   if (ARGV.length == 2)
     gm = Integer(ARGV[0])
     fp = Integer(ARGV[1])
-    
-    if (mode > 0 && mode <= 3) && (start > 0 && start <= 2)
-      @game = GameModel.new(mode, start)
-    end
+    start(GameModel.new(gm, fp)) if (gm >= 1 && gm <= 3) && (fp == 1 || fp == 2)
   end
   
   if (ARGV.length == 0)
-      gm = gp.for_game_mode
-      fp = gp.for_starting_player
-      @game = GameModel.new(gm, fp)
+    gm = gp.for_game_mode
+    fp = gp.for_starting_player
+    start(GameMode.new(gm, fp))
   end
-  
-  #loop through until the game over status is true
-  #until game.is_over()
-    #game.make_move()
-  #end
 end
