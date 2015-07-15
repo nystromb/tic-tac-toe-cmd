849d9f42d0e00026fdce77cbd5dcdc0b76582b76
nystromb <bnystrom@siu.edu>
2015-07-14 23:41:19 -0400

    broke away prompt code to GamePrompt class

---
+++ lib/GamePrompt.rb
@@ -0,0 +1,52 @@
+class Prompt
+  def initialize
+  end
+  
+  #prompts user for their desired game mode
+  def for_game_mode
+    #Helpful instructions for first time user:
+    puts "\nHere are your options:"
+    puts "Press 1 for 1 Player mode. "
+    puts "Press 2 for 2 Player mode. "
+    puts "Press 3 for an automated game.\n\n"
+
+    #Get user input for the game mode:
+    print "What do you want your game mode to be? "
+  
+    while true
+      mode = Integer(gets.chomp)
+      case mode
+      when 1..3
+        break
+      else
+        print "Please enter a valid game type: "
+      end
+    end
+    
+    return mode
+  end
+  
+  #prompts user for desired person to start
+  def for_starting_player
+    # Helpful instructions for options
+    puts "Here are some more options:"
+    puts "Press 1 to have Player1 start."
+    puts "Press 2 to have Player2 start.\n\n"
+    
+    # Get user input for whoever is going to have the first turn:
+    print "Now, select who gets to go first: "
+    start = Integer(gets.chomp)
+    while true
+      unless (start == 1) || (start == 2)
+        print "Invalid input, try again: "   
+        start = Integer(gets.chomp)
+      else
+        break
+      end
+    end
+    
+    return start
+  end
+  
+  
+end
