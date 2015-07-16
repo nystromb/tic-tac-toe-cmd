095ebffad4c5381ac26d7418d793fab9bc3d7f04
nystromb <bnystrom@siu.edu>
2015-07-15 22:12:43 -0400

    more tests, more organized

--- spec/GameBoard_spec.rb
+++ spec/GameBoard_spec.rb
@@ -1,11 +1,22 @@
 require "gameboard"
 
 RSpec.describe GameBoard do
-  describe '# New Game:' do
+  describe '# Initial game board:' do
     it 'All game board locations should be empty (nil)' do
       9.times { |i|  expect(GameBoard.new.is_empty?(i+1)).to eq(true) }
     end
-    it 'check for empty game board' do
+    it 'Should have no winning move' do
+      expect(GameBoard.new.winning_move?).to eq(false)
+    end
+  end
+  
+  describe '# Playing game moves' do
+    board = GameBoard.new
+    it 'place a game piece at location 1' do
+      board.play_move(1, "X")
+      expect(board.get_piece_at(1)).to eq("X")
+    end
+    it 'Confirm if the indexes are empty' do
       board = GameBoard.new
       
       expect(board.is_empty?(1)).to eq(true)
@@ -16,22 +27,23 @@ RSpec.describe GameBoard do
       board.play_move(9, "O")
       expect(board.is_empty?(9)).to eq(false)
     end
-    it 'fresh game board should have a no winning move' do
-      expect(GameBoard.new.winning_move?).to eq(nil)
-    end
   end
   
-  describe '# Playing game moves' do
-    board = GameBoard.new
-    it 'can place a game piece at location 1' do
-      board.play_move(1, "X")
-      expect(board.get_piece_at(1)).to eq("X")
-    end
-    it 'should detect a winning move' do
+  describe 'winning_move? function' do
+    it 'Should detect if there is a winning OOO move' do
+      board = GameBoard.new
       board.play_move(2, "O")
       board.play_move(5, "O")
       board.play_move(8, "O")
-      expect(board.winning_move?).to eq("O")
+      expect(board.winning_move?).to eq(true)
+    end
+    it 'Should detect a winning XXX move' do
+      board = GameBoard.new
+      board.play_move(1, "X")
+      board.play_move(5, "X")
+      board.play_move(9, "X")
+      board.to_s
+      expect(board.winning_move?).to eq(true)
     end
   end
 end
--- spec/GameModel_spec.rb
+++ spec/GameModel_spec.rb
@@ -1,11 +1,12 @@
 require "gamemodel"
 require "gameboard"
+require "gameprompt"
 require "players"
 
 RSpec.describe GameModel do
-  describe '# valid moves ' do
-    it 'Should only accept a move that is 1..9' do
-      gb = GameModel.new(1,1)
+  describe '# Check for valid moves ' do
+    gb = GameModel.new
+    it 'Only accept a move that is 1..9' do
       #1..9 should be true
       9.times { |i| expect(gb.move_is_valid(i+1)).to eq(true)}
       
@@ -14,9 +15,9 @@ RSpec.describe GameModel do
       
       #less than 1 shouldn't work either
       9.times { |i| expect(gb.move_is_valid(i-8)).to eq(false)}
-      
-      #if user inputs a string..
-      expect(gb.move_is_valid("string").to eq(false))
+    end
+    it 'should not accept a string as input' do
+      expect(gb.move_is_valid("string")).to eq(false)
     end
   end
   
