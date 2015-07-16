4df21a3075eafb533a588d1656f6d6f20a631833
nystromb <bnystrom@siu.edu>
2015-07-16 15:22:16 -0400

    shortened syntax and moves game loop to ../tictactoe.rb

--- lib/GameModel.rb
+++ lib/GameModel.rb
@@ -1,56 +1,39 @@
 class GameModel
-  def initialize(prompt = Prompt.new, player1 = Human.new, player2 = Human.new, mode = "Human v. Computer")
-    @gb = GameBoard.new
-    @prompt = prompt
-    @player1 = player1
-    @player2 = player2
+  attr_accessor :mode
+  attr_accessor :current_player
+  attr_accessor :total_moves
+  
+  def initialize(settings = GameInit.new)
+    @board = GameBoard.new
+    @player1, @player2 = settings.players
+    
+    @mode = settings.mode_to_s
     @total_moves = 0
     (@player1.starts) ? @current_player = @player1 : @current_player = @player2
-    
-    @prompt.intro(@current_player.name, mode)
-  end
-
-  #main method to make a move, if the user is a human, then it will prompt the user for their move
-  def make_move
-    @gb.to_s
-    
-    if @current_player.is_human?
-      move = @prompt.user_move(@current_player.name)
-      until move_is_valid(move)
-        print "> "
-        move = gets.chomp
-      end
-    else #computer must make a move
-      
-    end
-    
-    @gb.play_move(move.to_i, @current_player.peice)
-    @total_moves += 1
-    change_turns
   end
   
-  def change_turns
-    (@current_player == @player1) ? @current_player = @player2 : @current_player = @player1
+  def play(move)
+    @board.play_move(move.to_i, @current_player.peice)
+    change_turns
+    @total_moves += 1
   end
   
   #method to check if a given input move is legal or illegal
   def move_is_valid(input)
-    return @gb.is_empty?(input.to_i) 
+    return @board.is_empty?(input.to_i) 
   end
   
   #method returns true or false if game is over
   def is_over
-    result = false
+    return true if (@board.winning_move?) || (@total_moves == 9)
+    return false
+  end
     
-    # game is over if there's a winning move or there have been 9 game moves
-    if @gb.winning_move?
-      result = true
-      # prompt for win message
-    elsif @total_moves == 9
-      result = true
-      # @win_message = "Draw game!"
-    end
-    
-    return result
+  def change_turns
+    (@current_player == @player1) ? @current_player = @player2 : @current_player = @player1
+  end
+  
+  def board_print
+    @board.to_s
   end
 end
