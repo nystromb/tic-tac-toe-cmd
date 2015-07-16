517992d7105ac9b2bb3f2ed3557063ddb8d93d57
nystromb <bnystrom@siu.edu>
2015-07-15 20:28:50 -0400

    added check winning move & refactoring
    Changed the game board from array to a hash and added an array for the
    winning index combinations. Changed variable names for readability

--- lib/GameBoard.rb
+++ lib/GameBoard.rb
@@ -1,35 +1,47 @@
 class GameBoard
   def initialize
-    @game_board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+    @board = {1 => nil,2 => nil,3 => nil,4 => nil,5 => nil,6 => nil,7 => nil,8 => nil,9 => nil}
+    @win_combination = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   end
   
   #method to play a given move, and puts it on the game_board
-  def play_move(input, piece)
-    @game_board[input-1] = piece
+  def play_move(index, piece)
+    @board[index-1] = piece
   end
   
   #get the piece at the specified game board location
-  def get_piece_at(location)
-    return @game_board[location-1]
+  def get_piece_at(index)
+    return @board[index-1]
   end
   
   #check if the game location is empty
-  def is_empty?(location)
-    return true if @game_board[location-1].nil?
+  def is_empty?(index)
+    return true if (index > 0 && index <= 9) && (@board[index-1].nil?)
     return false
+  end
+  
+  #checks game board for a winning combination exists
+  def winning_move?
+    @win_combination.each do |index|
+      #check game board to see if XXX or OOO combination exists
+      if (@board[index[0]] == "X") && (@board[index[1]] == "X") && (@board[index[2]] == "X")
+        return "X"
+      elsif (@board[index[0]] == "O") && (@board[index[1]] == "O") && (@board[index[2]] == "O")
+        return "O"
+      end
+    end
+    return nil
   end
   
   #method prints out the current game board
   def to_s
-    count = 1
-    @game_board.each do |loc|
-      if loc.nil?
-        print " #{count} "
+    @board.each do |index, value|
+      if value.nil?
+        print " #{index} "
       else
-        print "#{loc} "
+        print "#{value} "
       end
-      print "\n" if (count % 3) == 0 
-      count += 1
+      print "\n" if (index % 3) == 0 
     end
   end
   
