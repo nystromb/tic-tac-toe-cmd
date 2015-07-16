031e17d6ae6960ff8fb2f785003581b18734e880
nystromb <bnystrom@siu.edu>
2015-07-16 15:23:05 -0400

    added basic, functional 2 player game loop
    computer AI doesn’t work just yet

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,33 +4,48 @@ require "./lib/GameModel.rb"
 require "./lib/GamePrompt.rb"
 require "./lib/GameInit.rb"
 
-def start(game)
+def start(prompt, game)
+  prompt.intro(game.current_player.name, game.mode)
+  
   until game.is_over
-    game.make_move
+    game.board_print
+    
+    if game.current_player.is_human?
+      move = prompt.user_move(game.current_player)
+      until game.move_is_valid(move)
+        print "> "
+        move = gets.chomp
+      end
+    else #computer must make a move
+      
+    end
+
+    game.play(move)
   end
+  
+  #game is now over, print final board
+  game.board_print
 end
 
 if __FILE__ == $0
   prompt = Prompt.new
-  init = GameInit.new(prompt)
+  init = GameInit.new
   
   #check for command line args
-  if (ARGV.length == 2) && (init.cmd_line_args(ARGV[0], ARGV[1]))
-    mode = ARGV[0].to_i
-    fp = ARGV[1].to_i
+  if (ARGV.length == 2) && (init.check_args(ARGV[0], ARGV[1]))
+    init.game_mode = ARGV[0].to_i
+    init.first_player = ARGV[1].to_i
     init.ready = true
   elsif (ARGV.length == 0) # prompt the user for input
-    mode = prompt.for_game_mode
-    fp = prompt.for_starting_player
+    init.game_mode = prompt.for_game_mode
+    init.first_player = prompt.for_starting_player
     init.ready = true
   end
   
   unless init.ready
     puts "USAGE MESSAGE"
   else
-    init.players(mode)
-    init.set_starting(fp)
-    start(GameModel.new(prompt, init.player1, init.player2, init.mode))
+    start(prompt, GameModel.new(init))
   end
   
 end
