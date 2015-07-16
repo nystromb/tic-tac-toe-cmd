bfe6b75f63f664b20a6b80c57de588738642f449
nystromb <bnystrom@siu.edu>
2015-07-16 15:19:38 -0400

    better test names, is_empty tests

--- spec/GameBoard_spec.rb
+++ spec/GameBoard_spec.rb
@@ -1,22 +1,22 @@
 require "gameboard"
 
 RSpec.describe GameBoard do
-  describe '# Initial game board:' do
-    it 'All game board locations should be empty (nil)' do
+  describe '# initialize:' do
+    it 'should be have all game board locations empty' do
       9.times { |i|  expect(GameBoard.new.is_empty?(i+1)).to eq(true) }
     end
-    it 'Should have no winning move' do
+    it 'should have no winning move' do
       expect(GameBoard.new.winning_move?).to eq(false)
     end
   end
   
-  describe '# Playing game moves' do
+  describe '# play_move and get_peice_at:' do
     board = GameBoard.new
-    it 'place a game piece at location 1' do
+    it 'can place a game piece at location' do
       board.play_move(1, "X")
       expect(board.get_piece_at(1)).to eq("X")
     end
-    it 'Confirm if the indexes are empty' do
+    it 'confirm if the indexes are empty' do
       board = GameBoard.new
       
       expect(board.is_empty?(1)).to eq(true)
@@ -27,22 +27,29 @@ RSpec.describe GameBoard do
       board.play_move(9, "O")
       expect(board.is_empty?(9)).to eq(false)
     end
+    it 'fill all spots and check is empty' do
+      board = GameBoard.new
+      9.times {|i| expect(board.is_empty?(i+1)).to eq(true)}
+      9.times {|i| expect(board.is_empty?(i+10)).to eq(false)}
+      9.times { |i| board.play_move(i+1,"X") }
+      9.times { |i| expect(board.is_empty?(i+1)).to eq(false)}
+      
+    end
   end
   
   describe 'winning_move? function' do
-    it 'Should detect if there is a winning OOO move' do
+    it 'should detect if there is a winning OOO move' do
       board = GameBoard.new
       board.play_move(2, "O")
       board.play_move(5, "O")
       board.play_move(8, "O")
       expect(board.winning_move?).to eq(true)
     end
-    it 'Should detect a winning XXX move' do
+    it 'should detect a winning XXX move' do
       board = GameBoard.new
       board.play_move(1, "X")
       board.play_move(5, "X")
       board.play_move(9, "X")
-      board.to_s
       expect(board.winning_move?).to eq(true)
     end
   end
