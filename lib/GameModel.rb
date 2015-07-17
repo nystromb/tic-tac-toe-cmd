79e6003a90e455cbb54a6174c320b4babd8c1edd
nystromb <bnystrom@siu.edu>
2015-07-17 01:55:32 -0400

    moved functions from GameBoard and GameInit

--- lib/GameModel.rb
+++ lib/GameModel.rb
@@ -1,47 +1,93 @@
 class GameModel
-  attr_accessor :mode
-  attr_accessor :current_player
-  attr_accessor :total_moves
-  attr_reader :last_move
+  attr_reader :mode
+  attr_reader :current_player
+  attr_reader :total_moves
+  attr_reader :winner
   
-  def initialize(settings = GameInit.new)
+  def initialize(mode = 2, sp = 1)
     @board = GameBoard.new
-    @player1, @player2 = settings.players
-    @last_move = nil
-    @mode = settings.mode
+    init_players(mode, sp)
     @total_moves = 0
-    (@player1.starts) ? @current_player = @player1 : @current_player = @player2
+    @winning_moves = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+    @winner = nil
+  end
+  
+  #method returns true or false if game is over
+  def is_over
+    return true if (winning_move?) || (@total_moves == 9)
+    return false
   end
   
   def play(move)
-    @board.play_move(move.to_i, @current_player.peice)
-    @last_move = @current_player
+    @board.place(move, @current_player.peice)
     @total_moves += 1
     change_turns
   end
   
   #method to check if a given input move is legal or illegal
   def move_is_valid(input)
-    return @board.is_empty?(input.to_i) 
+    return true if (input > 0 && input < 10) && (@board.get(input).nil?)
+    return false 
   end
   
-  #method returns true or false if game is over
-  def is_over
-    if (@board.winning_move?)
-      return true
-    elsif (@total_moves == 9)
-      @last_move = nil
-      return true
-    else
-      return false
-    end
-  end
-    
   def change_turns
-    (@current_player == @player1) ? @current_player = @player2 : @current_player = @player1
+    (@current_player == @p1) ? @current_player = @p2 : @current_player = @p1
   end
   
-  def board_print
-    @board.to_s
+  #method prints out the current game board
+  def to_s
+    print "\n"
+    @board.spots.each do |index, value|
+      (value.nil?) ? (print " #{index} ") : (print " #{value} ")
+      print "\n" if (index % 3) == 0 
+    end
+    print "\n"
+  end
+
+  #checks game board for a winning combination exists
+  def winning_move?
+    @winning_moves.each do |set|
+      #check game board to see if XXX or OOO combination exists
+      if (@board.match(set, @p1.peice))
+        @winner = @p1
+        return true
+      elsif(@board.match(set, @p2.peice))
+        @winner = @p2
+        return true
+      end
+    end
+    return false
+  end
+  
+  def init_players(mode, sp)
+    case mode
+    when 1
+      @mode = "Human v. Computer"
+      @p1 = Human.new
+      @p2 = Computer.new
+    when 2
+      @mode = "Human v. Human"
+      @p1 = Human.new
+      @p2 = Human.new
+    when 3
+      @mode = "Computer v. Computer"
+      @p1 = Computer.new
+      @p2 = Computer.new
+    else
+      puts "Could not create players"
+      exit
+    end
+
+    if (sp == 1)
+      @p1.starts = true
+      @p1.peice = "X"
+      @p2.peice = "O"
+    else
+      @p2.starts = true
+      @p2.peice = "O"
+      @p1.peice = "X"
+    end
+    
+    (@p1.starts) ? @current_player = @p1 : @current_player = @p2
   end
 end
