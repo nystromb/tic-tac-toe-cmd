c23aeb8d6461f5775cbfcf4c445ce1d3efe4a077
nystromb <bnystrom@siu.edu>
2015-07-27 02:39:20 -0500

    generate_move method

--- lib/Players.rb
+++ lib/Players.rb
@@ -1,7 +1,5 @@
 class Player
-  attr_accessor :name
-  attr_accessor :starts
-  attr_accessor :peice
+  attr_accessor :name, :starts, :peice
   
   def initialize (name = "Player")
     @starts = false
@@ -15,6 +13,7 @@ class Player
 end
 
 class Computer < Player
+  attr_accessor :choice
   def initialize(name = "Computer")
     @name = name
     @starts = false
@@ -22,6 +21,54 @@ class Computer < Player
   
   def is_human? 
     return false
+  end
+  
+  def generate_move(node, outcomes = {}, depth = 0)
+    return score(node, depth) if node.is_over?
+    
+    moves = node.get_empty_locs
+    moves.each do |move|
+      node.play(move)
+      new_node = node.clone
+      outcomes[move] = generate_move(new_node, outcomes, depth + 1)
+      node.play(move, nil)
+    end
+    
+    
+    if depth == 0
+      move = nil
+      if(node.current_player.peice == "X")
+        best = -100
+        outcomes.each do |m, score|
+          if (score.is_a? Integer) && (score > best)
+            move = m
+            best = score
+          end
+        end
+      else
+        best = 100
+        outcomes.each do |m, score|
+          if (score.is_a? Integer) && (score < best)
+            move = m
+            best = score
+          end
+        end
+      end
+      return move
+    else
+      #print "OUTCOMES #{outcomes}\n"
+      return outcomes
+    end
+  end
+  
+  def score(game, depth)
+    if game.winner == "X"
+      return depth - 10
+    elsif game.winner == "O"
+      return 10 - depth
+    elsif game.winner.nil?
+      return 0
+    end
   end
   
 end
