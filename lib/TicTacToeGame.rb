cf8c87710d591bce403ccbbffcf4b122a53f1887
nystromb <bnystrom@siu.edu>
2015-07-27 02:40:12 -0500

    player implementation moved

--- lib/TicTacToeGame.rb
+++ lib/TicTacToeGame.rb
@@ -1,45 +1,38 @@
 class TicTacToeGame
   
-    def initialize(mode = 2, sp = 1, game = GameModel.new)
-      @game = game
-      @m = mode
-      init_players(@m, sp)
-      start
+  def initialize(mode = 2, sp = 1, game = GameModel.new)
+    @game = game
+      
+    case mode
+    when 1
+      @mode = "Human v. Computer"
+      p1 = Player.new
+      p2 = Computer.new
+    when 2
+      @mode = "Human v. Human"
+      p1 = Player.new
+      p2 = Player.new
+    when 3
+      @mode = "Computer v. Computer"
+      p1 = Computer.new
+      p2 = Computer.new
+    end
+
+    if (sp == 1)
+      p1.starts = true
+      p1.peice = "X"
+      p2.peice = "O"
+    else
+      p2.starts = true
+      p2.peice = "X"
+      p1.peice = "O"
     end
     
-    def init_players(mode, sp)
-      case mode
-      when 1
-        @mode = "Human v. Computer"
-        @p1 = Player.new
-        @p2 = Computer.new
-      when 2
-        @mode = "Human v. Human"
-        @p1 = Player.new
-        @p2 = Player.new
-      when 3
-        @mode = "Computer v. Computer"
-        @p1 = Computer.new
-        @p2 = Computer.new
-      end
-
-      if (sp == 1)
-        @p1.starts = true
-        @p1.peice = "X"
-        @p2.peice = "O"
-      else
-        @p2.starts = true
-        @p2.peice = "X"
-        @p1.peice = "O"
-      end
-    
-      (@p1.starts) ? @game.current_player = @p1 : @game.current_player = @p2
+    @game.set_players(p1, p2)
+      
+    start
   end
   
-  def change_turns(game)
-    (game.current_player == @p1) ? game.current_player = @p2 : game.current_player = @p1
-  end
-
   def start(prompt = Prompt.new)
     prompt.intro(@game.current_player, @game.mode)
 
@@ -51,43 +44,27 @@ class TicTacToeGame
           move = prompt.user_move(@game.current_player)
         else
           prompt.computer_move
-          generate_move(@game)            
+          case @game.spots_filled
+          when 0
+            move = [1,3,7,9][Random.rand(4)]
+            break
+          when 1
+            move = 5 if @game.board[5].nil?
+            break
+          else
+            @game.get_empty_locs.each do |m|
+              move = m if @game.next_win?(m, @game.get_opponent.peice)
+              move = m if @game.next_win?(m, @game.current_player.peice)
+            end
+          end
+          move = @game.current_player.generate_move(@game.clone) if move.nil?
         end 
         break if @game.move_is_valid(move.to_i)
       end
-      @game.play(move.to_i, @game.current_player.peice)
-      change_turns(@game)
+      @game.play(move.to_i)
     end
     #game is now over, print final board 
     @game.to_s
     (@game.winner == nil) ? (prompt.game_over_msg) : (prompt.game_over_msg(@game.winner))
-  end
-
-  def generate_move(node, outcomes = {}, depth = 0)
-    return score(node, depth) if node.is_over?
-    puts node.inspect
-    moves = node.get_empty_locs
-    
-    moves.each do |move|
-      (node.spots_filled.even?) ? node.play(move, "X") : node.play(move, "O")
-      node = GameModel.new(node.board)
-      node.to_s
-      outcomes[move] = generate_move(node, outcomes, depth + 1)
-      node.play(move, nil)
-    end
-    
-    print "OCS: #{outcomes} \n"
-  end
-  
-  def score(game, depth)
-    puts "GAME WINNER: #{game.winner}"
-    case game.winner
-    when "X"
-      return 10 - depth
-    when "O"
-      return depth - 10
-    when nil
-      return 0
-    end
   end
 end
