8e33373c10bacb2d4e9d11206f178fcf44364553
nystromb <bnystrom@siu.edu>
2015-07-28 02:15:23 -0500

    Added retry? loop

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,21 +1,30 @@
-require "./lib/Players.rb"
-require "./lib/GameModel.rb"
+require "./lib/Player.rb"
+require "./lib/Computer.rb"
+require "./lib/GameBoard.rb"
 require "./lib/GamePrompt.rb"
-require "./lib/TicTacToeGame.rb"
+require "./lib/GameModel.rb"
 
 if __FILE__ == $0
-  prompt = Prompt.new
-  ready = false
-  #check for command line args
-  if (ARGV.length == 2)
-    mode = ARGV[0].to_i
-    fplyr = ARGV[1].to_i
-    ((fplyr == 1 || fplyr == 2) && (mode >= 1 && mode <= 3)) ? ready = true : (prompt.usage)
-  elsif (ARGV.length == 0) # prompt the user for input
-    mode = prompt.for_game_mode
-    fplyr = prompt.for_starting_player
-    ready = true
-  end
   
-  TicTacToeGame.new(mode, fplyr) if ready
+  loop do 
+    prompt = Prompt.new
+    ready = false
+    fplyr = nil
+    mode = nil
+    #check for command line args
+    if (ARGV.length == 2)
+      mode = ARGV[0].to_i
+      fplyr = ARGV[1].to_i
+      ((fplyr == 1 || fplyr == 2) && (mode >= 1 && mode <= 3)) ? ready = true : (prompt.usage)
+    elsif (ARGV.length == 0) # prompt the user for input
+      mode = prompt.for_game_mode
+      fplyr = prompt.for_starting_player
+      ready = true
+    end
+  
+    GameModel.new(mode, fplyr) if ready
+    
+    break if !(prompt.retry?)
+    ready = false
+  end
 end
