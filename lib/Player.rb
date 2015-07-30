e26de7afbe4e01538ebceb3267ce15dea893c537
nystromb <bnystrom@siu.edu>
2015-07-30 18:06:07 -0500

    Added a human attribute

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,13 +1,9 @@
 class Player
-  attr_accessor :name, :starts, :peice
+  attr_accessor :name, :starts, :peice, :human
   
   def initialize (name = "Player")
     @starts = false
+    @human = true
     @name = name
-  end
-  
-  #player by default is human
-  def is_human?
-    return true
   end
 end
