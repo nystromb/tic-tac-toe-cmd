5ef394b2d4f1a3882e4d700736e0dd2cd85d7df0
nystromb <bnystrom@siu.edu>
2015-07-30 18:03:38 -0500

    Moved state functionality to GameModel

--- lib/GameBoard.rb
+++ lib/GameBoard.rb
@@ -7,21 +7,14 @@ class GameBoard
     @winner
   end
   
-  #sets both players for the game
-  def set_players(player1, player2)
-    @p1, @p2 = player1, player2
-    (@p1.starts) ? @current_player = @p1 : @current_player = @p2
-  end
-  
   #method returns true or false if game is over
   def is_over?
     ((winning_move?) || (spots_filled == 9)) ? (return true) : (return false)
   end
   
   #places the game peice on the given location
-  def play(move, peice = @current_player.peice)
+  def play(move, peice)
     @board[move] = peice
-    change_turns
   end
   
   #returns all the empty locations on the board.
@@ -33,13 +26,7 @@ class GameBoard
   
   #method to check if a given input move is legal or illegal
   def move_is_valid(index)
-    return true if ((index >= 1) && (index <= 9)) && (@board[index].nil?)
-    return false 
-  end
-  
-  #cycles the current player's turn
-  def change_turns
-    (@current_player == @p1) ? @current_player = @p2 : @current_player = @p1
+    ((index >= 1) && (index <= 9)) && (@board[index].nil?) ? (return true) : (return false) 
   end
   
   #checks game board for a winning combination exists
@@ -63,5 +50,4 @@ class GameBoard
     @board.each { |index, spot| (spot.nil?) ? next : count += 1 }
     return count
   end
-  
 end
